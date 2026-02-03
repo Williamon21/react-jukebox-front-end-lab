f5bad3af5b0d812d84788c75ce3ac9f46987fa7f
dlswilliam <santo@Project2.localdomain>
2026-02-03 16:18:13 -0500

    trackServices

--- src/services/trackService.js
+++ src/services/trackService.js
@@ -1,1 +1,81 @@
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tracks`;
+
+// Get all tracks from the back end server
+const index = () => {
+  // Old school way of doing async await, used then and callbacks
+  return fetch(BASE_URL)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+// create a track
+const create = async (track) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(pet),
+    });
+    const data = await res.json();
+    return data.pet;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// update a track
+const update = async (formData, trackId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${trackId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    return data.track;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// delete a track by id
+const deleteTrack = async (trackId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${trackId}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    return data.track;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { index, create, update, deleteTrack };
+
+// currently playing track
+
+const playingTrack = async (trackId) => {
+    try {
+        const res = await fetch(`${BASE_URL}/playing/${trackId}`, {
+            method: "GET",
+        });
+        const data = await res.json();
+        return data.track;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export { playingTrack };
